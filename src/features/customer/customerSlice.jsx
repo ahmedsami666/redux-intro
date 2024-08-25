236bd78e24cbb9ed8ce90b1fb57855c8bcb6a8c3
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-08-25 16:19:55 +0300

    replacing redux with redux toolkit

--- src/features/customer/customerSlice.jsx
+++ src/features/customer/customerSlice.jsx
@@ -1,10 +1,39 @@
-const initialStateCutomer = {
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
     fullName: '',
     nationalID: '',
     createdAt: ''
 }
 
-export default function customerReducer(state = initialStateCutomer, action) {
+const customerSlice = createSlice({
+    name: 'customer',
+    initialState: initialState,
+    reducers: {
+        createCustomer: {
+            prepare(fullName, nationalID) {
+                return {
+                    payload: { fullName, nationalID, createdAt: new Date().toISOString() }
+                }
+            },
+            reducer(state, action) {
+                state.fullName = action.payload.fullName
+                state.nationalID = action.payload.nationalID
+                state.createdAt = action.payload.createdAt
+            }
+        },
+        updateName(state, action) {
+            state.fullName = action.payload
+        }
+    }
+})
+
+export const { createCustomer, updateName } = customerSlice.actions
+
+export default customerSlice.reducer
+
+
+/*export default function customerReducer(state = initialStateCutomer, action) {
     switch (action.type) {
         case 'customer/createCustomer':
             return {
@@ -35,4 +64,4 @@ export function updateName(fullName) {
         type: 'customer/updateName',
         payload: fullName
     }
-}
+}*/
